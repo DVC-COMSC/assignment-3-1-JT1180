828abc1eb980781e8390107879a462345d6e5eed
JT_1180 <126540229+JT1180@users.noreply.github.com>
2023-03-15 16:02:43 +0000

    Completed assignment 3-1

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+  import random
+  num1 = random.randint(0,100)
+  print(num1)
+  num2 = random.randint(0,100)
+  print(num2)
+  num3 = random.randint(0,100)
+  print(num3)
 
+  if (num1 <= num2) and (num1 <= num3):
+    smallest = num1
+  elif (num2 <= num1) and (num2 <= num3):
+     smallest = num2
+  else:
+    smallest = num3
+  print('The smallest number is: ',smallest)
 
 if __name__ == '__main__':
     main()
